a37b74c47665c000a8feffabbd249ae879d503de
scbennion <sambennion2005@gmail.com>
2026-06-26 08:30:36 -0600

    Implemented resetBoard function

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -43,7 +43,7 @@ public class ChessBoard {
      * (How the game of chess normally starts)
      */
     public void resetBoard() {
-        throw new RuntimeException("Not implemented");
+        squares = new ChessPiece[size][size];
     }
 
     public int getSize() {
