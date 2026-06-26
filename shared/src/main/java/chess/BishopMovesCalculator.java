1853135e0f58958f9a1afefec055d937a1d25938
scbennion <sambennion2005@gmail.com>
2026-06-25 22:28:43 -0600

    Simplified pieceMoves function for BishopMovesCalculator and completed functionality for BishopMoves

--- shared/src/main/java/chess/BishopMovesCalculator.java
+++ shared/src/main/java/chess/BishopMovesCalculator.java
@@ -8,24 +8,39 @@ public class BishopMovesCalculator implements PieceMovesCalculator{
     @Override
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
         Collection<ChessMove> moves = new ArrayList<>();
-        int col = myPosition.getColumn();
-        int row = myPosition.getRow();
+        moves.addAll(computeDir(1,1, board, myPosition));
+        moves.addAll(computeDir(1,-1, board, myPosition));
+        moves.addAll(computeDir(-1,-1, board, myPosition));
+        moves.addAll(computeDir(-1,1, board, myPosition));
+        return moves;
+    }
 
-        for (int c = 1; c <= board.getSize(); c++) {
-            int northEastRow = row + (c - col); //NE
-            if (northEastRow <= board.getSize() && northEastRow >= 1) {
-                if (board.getPiece(new ChessPosition(northEastRow, c)) == null) {
-                    moves.add(new ChessMove(myPosition, new ChessPosition(northEastRow, c), null));
-                }
-            }
-
-            int southEastRow = row - (c - col); //SE
-            if (southEastRow >= 1 && southEastRow <= board.getSize()) {
-                if (board.getPiece(new ChessPosition(southEastRow, c)) == null) {
-                    moves.add(new ChessMove(myPosition, new ChessPosition(southEastRow, c), null));
-                }
+    /*
+    Check the valid moves for a piece in a given direction
+     */
+    private ArrayList<ChessMove> computeDir(int rowDir, int colDir, ChessBoard board, ChessPosition pos) {
+        int size = board.getSize();
+        ArrayList<ChessMove> moves = new ArrayList<>();
+        ChessGame.TeamColor color = board.getPiece(pos).getTeamColor();
+        int col = pos.getColumn();
+        int row = pos.getRow();
+        while (col + colDir >= 1 && col + colDir <= size && row + rowDir >= 1 && row + rowDir <= size) {
+            col += colDir;
+            row += rowDir;
+            ChessPosition newPos = new ChessPosition(row, col);
+            ChessPiece p = board.getPiece(newPos);
+            ChessMove m = new ChessMove(pos, newPos, null);
+            if (p == null) { //empty square
+                moves.add(m);
+            } else if (p.getTeamColor() == color) { //blocked by my piece
+                break;
+            } else { //blocked by enemy piece
+                moves.add(m);
+                break;
             }
         }
         return moves;
+
+
     }
 }
