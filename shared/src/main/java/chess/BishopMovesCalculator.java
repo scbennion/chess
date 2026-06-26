bd80b3ea6ea4901692cf2a4762d82b428ff4f1c1
scbennion <sambennion2005@gmail.com>
2026-06-25 22:36:51 -0600

    Moved computeDir function to PieceMovesCalculator, allowing for future pieces to use the function

--- shared/src/main/java/chess/BishopMovesCalculator.java
+++ shared/src/main/java/chess/BishopMovesCalculator.java
@@ -8,39 +8,10 @@ public class BishopMovesCalculator implements PieceMovesCalculator{
     @Override
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
         Collection<ChessMove> moves = new ArrayList<>();
-        moves.addAll(computeDir(1,1, board, myPosition));
-        moves.addAll(computeDir(1,-1, board, myPosition));
-        moves.addAll(computeDir(-1,-1, board, myPosition));
-        moves.addAll(computeDir(-1,1, board, myPosition));
+        moves.addAll(PieceMovesCalculator.computeDir(1,1, board, myPosition));
+        moves.addAll(PieceMovesCalculator.computeDir(1,-1, board, myPosition));
+        moves.addAll(PieceMovesCalculator.computeDir(-1,-1, board, myPosition));
+        moves.addAll(PieceMovesCalculator.computeDir(-1,1, board, myPosition));
         return moves;
-    }
-
-    /*
-    Check the valid moves for a piece in a given direction
-     */
-    private ArrayList<ChessMove> computeDir(int rowDir, int colDir, ChessBoard board, ChessPosition pos) {
-        int size = board.getSize();
-        ArrayList<ChessMove> moves = new ArrayList<>();
-        ChessGame.TeamColor color = board.getPiece(pos).getTeamColor();
-        int col = pos.getColumn();
-        int row = pos.getRow();
-        while (col + colDir >= 1 && col + colDir <= size && row + rowDir >= 1 && row + rowDir <= size) {
-            col += colDir;
-            row += rowDir;
-            ChessPosition newPos = new ChessPosition(row, col);
-            ChessPiece p = board.getPiece(newPos);
-            ChessMove m = new ChessMove(pos, newPos, null);
-            if (p == null) { //empty square
-                moves.add(m);
-            } else if (p.getTeamColor() == color) { //blocked by my piece
-                break;
-            } else { //blocked by enemy piece
-                moves.add(m);
-                break;
-            }
-        }
-        return moves;
-
-
     }
 }
--- shared/src/main/java/chess/PieceMovesCalculator.java
+++ shared/src/main/java/chess/PieceMovesCalculator.java
@@ -1,7 +1,35 @@
 package chess;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 public interface PieceMovesCalculator {
     Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition);
+
+    /*
+    Check the valid moves for a piece in a given direction
+     */
+    static ArrayList<ChessMove> computeDir(int rowDir, int colDir, ChessBoard board, ChessPosition pos) {
+        int size = board.getSize();
+        ArrayList<ChessMove> moves = new ArrayList<>();
+        ChessGame.TeamColor color = board.getPiece(pos).getTeamColor();
+        int col = pos.getColumn();
+        int row = pos.getRow();
+        while (col + colDir >= 1 && col + colDir <= size && row + rowDir >= 1 && row + rowDir <= size) {
+            col += colDir;
+            row += rowDir;
+            ChessPosition newPos = new ChessPosition(row, col);
+            ChessPiece p = board.getPiece(newPos);
+            ChessMove m = new ChessMove(pos, newPos, null);
+            if (p == null) { //empty square
+                moves.add(m);
+            } else if (p.getTeamColor() == color) { //blocked by my piece
+                break;
+            } else { //blocked by enemy piece
+                moves.add(m);
+                break;
+            }
+        }
+        return moves;
+    }
 }
