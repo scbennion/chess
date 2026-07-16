765a9c5e41b78365ecf56ea0721d565b5c59e87f
scbennion <sambennion2005@gmail.com>
2026-07-15 18:19:42 -0600

    directed endpoints to handlers

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -11,6 +11,12 @@ public class Server {
         ChessHandler handler = new ChessHandler();
         javalin = Javalin.create(config -> config.staticFiles.add("web"))
                 .post("/user", handler::processRegister)
+                .post("/session", handler::processLogin)
+                .delete("/session", handler::processLogout)
+                .get("/game", handler::processListGames)
+                .post("/game", handler::processCreateGame)
+                .put("/game", handler::processJoinGame)
+                .delete("/db", handler::processClear)
                 .exception(Exception.class, handler::exceptionHandler);
         // Register your endpoints and exception handlers here.
 
