6702cd880acbe40a8bc1a9e019be3ca052088619
scbennion <sambennion2005@gmail.com>
2026-07-21 12:03:47 -0600

    setup for phase 4

---
+++ server/src/main/java/dataaccess/SQLAuthDAO.java
@@ -0,0 +1,25 @@
+package dataaccess;
+
+import model.AuthData;
+
+public class SQLAuthDAO implements AuthDAO {
+    @Override
+    public AuthData createAuth(String username) {
+        return null;
+    }
+
+    @Override
+    public AuthData getAuth(String authToken) {
+        return null;
+    }
+
+    @Override
+    public void deleteAuth(String authToken) {
+
+    }
+
+    @Override
+    public void clear() {
+
+    }
+}
