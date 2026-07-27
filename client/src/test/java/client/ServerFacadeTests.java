1cb8bca2b8f78d8691dccf5c38cbb75450666edc
scbennion <sambennion2005@gmail.com>
2026-07-26 19:16:27 -0700

    positive register test added

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -1,18 +1,23 @@
 package client;
 
+import model.UserData;
 import org.junit.jupiter.api.*;
 import server.Server;
+import server.ServerFacade;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ServerFacadeTests {
 
     private static Server server;
+    private static ServerFacade serverFacade;
 
     @BeforeAll
     public static void init() {
         server = new Server();
         var port = server.run(0);
         System.out.println("Started test HTTP server on " + port);
+        serverFacade = new ServerFacade(port);
     }
 
     @AfterAll
@@ -20,10 +25,21 @@ public class ServerFacadeTests {
         server.stop();
     }
 
+//    @BeforeEach
+//    void clearDatabase() {
+//        serverFacade.clear();
+//    }
+
+    @Test
+    void register() throws Exception {
+        var authData = serverFacade.register(new UserData("natalie", "p", "me@email.com"));
+        assertTrue(authData.authToken().length() > 10);
+    }
+
 
     @Test
     public void sampleTest() {
-        Assertions.assertTrue(true);
+        assertTrue(true);
     }
 
 }
