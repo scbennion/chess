e95845616dd0809d01e18231d8e0b5a1ea4c44e0
scbennion <sambennion2005@gmail.com>
2026-08-05 17:16:14 -0600

    added implementation for connection manager

--- server/src/main/java/server/ConnectionManager.java
+++ server/src/main/java/server/ConnectionManager.java
@@ -3,25 +3,35 @@ package server;
 import org.eclipse.jetty.websocket.api.Session;
 
 import java.io.IOException;
+import java.util.HashSet;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class ConnectionManager {
-    public final ConcurrentHashMap<Integer, Set<Session>> connections = new ConcurrentHashMap<>();
+    private ConcurrentHashMap<Integer, Set<Session>> connections = new ConcurrentHashMap<>();
 
     public void add(int gameID, Session session) {
-//        connections.put(gameID, session);
+        connections.computeIfPresent(gameID, (k, sessions) -> {
+            sessions.add(session);
+            return sessions;
+        });
+
+        connections.computeIfAbsent(gameID, k -> {
+            HashSet<Session> set = new HashSet<Session>();
+            set.add(session);
+            return set;
+        });
     }
 
     public void remove(int gameID, Session session) {
+        connections.remove(gameID);
     }
 
     public void broadcast(int gameID, Session excludeSession, String notification) throws IOException {
-        String msg = notification;
-        for (Session c : connections.get(gameID)) {
-            if (c.isOpen()) {
-                if (!c.equals(excludeSession)) {
-                    c.getRemote().sendString(msg);
+        for (Session s : connections.get(gameID)) {
+            if (s.isOpen()) {
+                if (!s.equals(excludeSession)) {
+                    s.getRemote().sendString(notification);
                 }
             }
         }
